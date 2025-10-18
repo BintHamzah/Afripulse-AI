fc7c039816542c57143accd43902683bd96908b0
Fatimah Hamzah <hamzahadetutu@gmail.com>
2025-10-18 15:06:52 +0100

    updating the Keys

--- src/sumarizeNews.py
+++ src/sumarizeNews.py
@@ -1,8 +1,21 @@
-import streamlit as st
+import os
 from openai import OpenAI
 
+#loading the API keys for Git, local and stramlit
+try:
+    import streamlit as st
+    api_key = st.secrets.get("OPENAI_API_KEY", None)
+except Exception:
+    api_key = None
 
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+if not api_key:
+    api_key = os.getenv("OPENAI_API_KEY")
+
+if not api_key:
+    raise ValueError("Missing OPENAI_API_KEY. Please set it in Streamlit Secrets or as an environment variable.")
+
+client = OpenAI(api_key=api_key)
+
 
 def summarize_article(title, content):
     prompt = f"Summarize this African startup news article in 300 words:\n\nTitle: {title}\n\nContent: {content}"
